a55e2902fabb51bc11ff86d5224dcc41d3533e0d
inging <patcharisa2004@gmail.com>
2023-11-11 13:02:12 +0700

    Initial commit

---
+++ int101_hw2_66130500067/src/main/java/work02/Person.java
@@ -0,0 +1,51 @@
+package work02;
+
+import work01.Utilitor;
+
+public class Person {
+
+    private static int nextId = 1; 
+    private final int id; //2.3
+    private String firstname; 
+    private String lastname; 
+
+    public Person(String firstname, String lastname) { 
+        this.firstname = Utilitor.testString(firstname);
+        this.lastname = Utilitor.testString(lastname);
+        this.id = Person.nextId++;
+
+    }
+
+    @Override
+    public String toString() { 
+        return "Person(" +id+","+firstname+","+lastname+ ')';
+    }
+
+   
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstname() { 
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setFirstname(String firstname) { 
+        this.firstname = Utilitor.testString(firstname);
+    }
+
+    public void setLastname(String lastname) { 
+        this.lastname = Utilitor.testString(lastname);
+    }
+
+    @Override
+    public boolean equals(Object obj) { 
+        return this == obj; 
+    }
+
+}
